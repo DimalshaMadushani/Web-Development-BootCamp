f88dd68eea16a537017590f76c02dd8fb10b7fc5
dimalsha <madushaniagd@gmail.com>
2024-01-12 11:20:03 +0530

    section-28
    step-1

--- section-28/TVShowSearch/app.js
+++ section-28/TVShowSearch/app.js
@@ -1,21 +1,30 @@
 const searchButton = document.querySelector('#searchButton')
 const form = document.querySelector('#form')
+const container2 = document.querySelector("#container2")
 
 form.addEventListener('submit',async function (e) {
     e.preventDefault();
     //remove all the images from the page after submiiting new search
     removeImages()
+    
     const searchTerm = form.elements.query.value
     //you can use the config which is from axios to apply the query
     const config = {params:{q:searchTerm}}
-    const res = await axios.get(`https://api.tvmaze.com/search/shows`,config)
-    // const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${input}`)
-    console.log(res.data[0].show.image.medium)
+    try{
+        const res = await axios.get(`https://api.tvmaze.com/search/shows`,config)
+        // const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${input}`)
+        console.log(res.data[0].show.image.medium)
+        console.log(res.data[0].show.name)
+        
+        displayImages(res.data)
+          // form.elements.query.value = '';
+    }
+    catch(e){
+        
+        document.getElementById("pasan").innerHTML += `<h3 class="text-danger mt-5 text-center display:none">Not Found</h3>`
     
-    displayImages(res.data)
-    //reset the search bar
-   // form.elements.query.value.reset(); not working
-  // form.elements.query.value = '';
+         
+    }
     
 })
 
@@ -25,11 +34,23 @@ function displayImages(pics) {
         if(pic.show.image){
             const img = document.createElement('img');
             img.src = pic.show.image.medium;
-            document.body.append(img)
-        }
+            console.log(img.src);
+            console.log(document.querySelector("#pasan"));
+            document.getElementById("pasan").innerHTML += `
         
+                <div class="col-12 col-sm-6 col-lg-3 mt-3">
+                <div class="card p-4 ps-5 pe-5">
+                    <h4 class="text-capitalize text-center">${pic.show.name}</h4>
+                    <img src="${img.src}" width="100%" height="250px" class="mx-auto d-block img-fluid"/>
+                    <button class="btn btn-info">more</button>
+                </div>
+                </div>
+            `
+            //document.body.append(img)
+        }
+       
     }
-    console.log(document.body.img)
+ //   console.log(document.body.img)
    
  }
 
@@ -40,6 +61,16 @@ const removeImages = () => {
         for(let i=0;i<imgTags.length;i++) {
             document.body.removeChild(imgTags[i]) ;
             }
+
+        
             
+        
     
+}
+
+const clearContainer = (cName) => {
+    const container = document.querySelector(cName);
+    console.log(container)
+    if(container.innerHTML != "")
+        container.remove();
 }
