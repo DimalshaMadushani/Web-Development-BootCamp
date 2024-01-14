d27a42db8a971d0d15bb7347cec41846b78f1869
dimalsha <madushaniagd@gmail.com>
2024-01-15 01:51:18 +0530

    section-28-flip-card

--- section-28/TVShowSearch/app.js
+++ section-28/TVShowSearch/app.js
@@ -18,7 +18,9 @@ form.addEventListener('submit',async function (e) {
         // const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${input}`)
         console.log(res.data[0].show.image.medium)
         console.log(res.data[0].show.name)
-        
+        //json objects
+        console.log(res.data);
+        //console.log(res.data[0].show.summary);
         displayImages(res.data)
           // form.elements.query.value = '';
     
@@ -31,24 +33,39 @@ form.addEventListener('submit',async function (e) {
     
 })
 
-function displayImages(pics) {
+function displayImages(results) {
 
-    for(let pic of pics){
+    for(let result of results){
         const img = document.createElement('img');
-        if(!pic.show.image){
+        if(!result.show.image){
             img.src = "https://school.cistercian.org/wp-content/uploads/connections-images/breianna-bairrington/no-image-available.jpg";
         } else {
-            img.src = pic.show.image.medium;
+            img.src = result.show.image.medium;
         }
+
+        let rating = getRating(result);
+        let duration = getDuration(result);
+
         document.getElementById("pasan").innerHTML += `
-        
-                <div class="col-12 col-sm-6 col-lg-3 mt-3">
-                <div class="card p-4 ps-5 pe-5">
-                    <h4 class="text-capitalize text-center">${pic.show.name}</h4>
-                    <img src="${img.src}" width="100%" height="250px" class="mx-auto d-block img-fluid"/>
-                    <button class="btn btn-info">more</button>
+       
+                <div class=" col-12 col-sm-6 col-lg-3 mt-3">
+                <div class = "card-container ">
+                <div class="card card-front p-4 ps-5 pe-5 ">
+                    <h4 class="text-capitalize text-center">${result.show.name}</h4>
+                    <img src="${img.src}" width="100%" height="250px" class="mx-auto d-block img-fluid mb-5"/>
+                    <p class="text-right">Rating: ${rating}</p>
+                    <p class="text-right">Duration: ${duration}</p>
+                </div>
+
+                <div class="card card-back p-4 ps-5 pe-5 ">
+                    <h4 class="text-capitalize text-center text-danger fw-bolder mb-2">SYNOPSIS</h4>
+                    <span id="summary" class=" summary text-start ">${result.show.summary}</span>
+                    <button id="read" class="btn btn-success mt-2">Continue reading</button>
+                    
                 </div>
                 </div>
+                </div>
+              
             `
        
     }
@@ -72,3 +89,18 @@ const clearResults = () => {
         container.append(newContainer);
 }
 
+const getRating = (data) => {
+    if(data.show.rating.average) {
+        return data.show.rating.average;
+    } else {
+        return "Not Available";
+    }
+}
+
+const getDuration = (data) => {
+    if(data.show.genres[2]) {
+        return data.show.genres[2];
+    } else {
+        return "Not Available";
+    }
+}
