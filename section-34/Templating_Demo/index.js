96630cd60458a97cf3ab5be7747ecc96df355c3b
dimalsha <madushaniagd@gmail.com>
2024-01-24 00:09:06 +0530

    section-34-step-2
    EJS, static_files and partials in Bootstarp_demo

--- section-34/Templating_Demo/index.js
+++ section-34/Templating_Demo/index.js
@@ -3,9 +3,14 @@ const app = express();
 const path = require('path');
 const redditData = require('./data.json');
 console.log(redditData);    
+// This method mounts the specified middleware function(s). In this context,
+// it's used to tell the Express.js app that you want the static files middleware to be applied to all incoming requests.
+app.use(express.static(path.join(__dirname,'static_files')));
 
 //behind the scenes express will require ejs and set it up for us
 app.set('view engine','ejs')
+//__dirname: This is a Node.js global variable 
+//that gives you the absolute path of the directory containing the currently executing file.
 app.set('views',path.join(__dirname,'/views'))
 
 app.get('/',(req,res) => {
