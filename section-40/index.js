86390f091365f6ebb9c70d80e0187fa79d812b7d
dimalsha <madushaniagd@gmail.com>
2024-03-03 12:00:29 +0530

    section-42  -step-1
    Creating custom error class
    With error handling middleware

--- section-40/index.js
+++ section-40/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const morgan = require('morgan')
+const AppError = require('./AppError')
 
 //using morgan-logger middleware
 app.use(morgan('tiny'))
@@ -15,18 +16,20 @@ app.use((req,res,next) => {
 
 
 //password middleware demo
-app.use((req,res,next) => {
-    console.log(req.query)
+const verifyPassword = (req,res,next) => {
+    // console.log(req.query)
     const {password} = req.query;
     if (password === 'catto'){
         console.log("catto right")
         next();
     }
     else {
-        return res.send("Sorry you need a password")
+        res.status(401)
+        throw new AppError('passsword required !!',401)
+        // return res.send("Sorry you need a password")
     }
    
-})
+}
 
 app.use('/dogs',(req,res,next) => {
     console.log("I love dogs ")
@@ -43,22 +46,35 @@ app.get('/dogs',(req,res) => {
     console.log(`REQUEST DATE : ${req.requestTime}`)
     res.send("woof woof")
 })
-
-app.get('/secret',(req,res) => {
+app.get('/error',(req,res) => {
+    chicken.fly();
+})
+// pass the middleware functions as cal backs for only this route to protect tht route
+app.get('/secret',verifyPassword,(req,res) => {
     res.send("My secret is i love catsss")
 })
 
-
+//demo for 403 status code
+app.get('/admin',(req,res) => {
+    throw new AppError('You are not an admin !',403)
+})
 app.use((req,res) => {
     res.status(404).send('NOT FOUND')
 })
 
+// //error handling middleware
+// app.use((err,req,res,next) => {
+//     console.log("***********error************")
+//     // res.status(500).send("oh boy! we got an error")
+//     next(err)
+// })
 
+//error handling middleware
+app.use((err,req,res,next) => {
+    const {status = 500, message = 'Something went wrong'} = err;
+    res.status(status).send(message)
+})
 
-
-
-
-
-app.listen(3000,() => {
-    console.log("App is running on localhost:3000")
+app.listen(3001,() => {
+    console.log("App is running on localhost:3001")
 })
